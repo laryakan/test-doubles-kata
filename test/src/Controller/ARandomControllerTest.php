43b4df89f892272505ab521fd084e84918828ed2
William PAUL <william.paul@altran.com>
2019-03-05 16:11:35 +0100

    Annot

--- test/src/Controller/ARandomControllerTest.php
+++ test/src/Controller/ARandomControllerTest.php
@@ -10,13 +10,17 @@ class ARandomControllerTest extends PHPUnit_Framework_TestCase
 
     // Au moins comme ça on aura toujours au moins un test
     // qui marche... en théorie
-    /** @test **/
+    /** @test
+    *** @coversNothing
+    **/
     public function assertTrueTest()
     {
         $this->assertTrue(true);
     }
 
-    /** @test **/
+    /** @test
+    ***
+    **/
     public function constructTest()
     {
         $arc = new ARandomController();
