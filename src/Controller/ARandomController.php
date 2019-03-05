0e39bc37ea762b8f5cdaa495cc20d28a60efcc29
William PAUL <william.paul@altran.com>
2019-03-05 16:08:28 +0100

    Moar tests, moar struct

--- src/Controller/ARandomController.php
+++ src/Controller/ARandomController.php
@@ -1,7 +1,18 @@
 <?php
+/*
+** Une classe quelconque qui fera notre sujet de test
+**
+**/
+
 
 namespace App\Controller;
 
 class ARandomController
 {
+    public $constructArgs;
+
+    public function __construct($arg1 = null, $arg2 = "foo", $arg3 = "bar")
+    {
+        $constructArgs= func_get_args();
+    }
 }
---
+++ test/src/Controller/ARandomControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Test\Controller;
+
+use App\Controller\ARandomController;
+use PHPUnit_Framework_TestCase;
+
+class ARandomControllerTest extends PHPUnit_Framework_TestCase
+{
+
+    // Au moins comme ça on aura toujours au moins un test
+    // qui marche... en théorie
+    /** @test **/
+    public function assertTrueTest()
+    {
+        $this->assertTrue(true);
+    }
+
+    /** @test **/
+    public function constructTest()
+    {
+        $arc = new ARandomController();
+        $this->assertInstanceOf('App\Controller\ARandomController', $arc);
+    }
+}
